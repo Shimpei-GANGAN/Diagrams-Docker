fb2221caff8424280fe066f1b232b65975ef1848
Shimpei Iwamaru <gangan.education@gmail.com>
2020-04-24 14:23:32 +0900

    add diagram.py

--- tmp/diagram.py
+++ tmp/diagram.py
@@ -1,7 +1,19 @@
-from diagrams import Diagram
-from diagrams.aws.compute import EC2
-from diagrams.aws.database import RDS
-from diagrams.aws.network import ELB
+from diagrams import Diagram, Cluster
+from diagrams.gcp.compute import Run
+from diagrams.gcp.devtools import Build, GCR, SourceRepositories as GSR
+from diagrams.gcp.database import SQL
+from diagrams.onprem.vcs import Github
+from diagrams.onprem.client import Client, User
 
-with Diagram("Web Service", show=False):
-    ELB("lb") >> EC2("web") >> RDS("userdb")
+with Diagram("Test", show=False):
+    with Cluster("Google Cloud Platform"):
+        with Cluster("CI/CD"):
+            dev = GSR("source mirror")
+            cicd = dev >> Build("build") >> GCR("container deploy")
+
+        with Cluster("Services"):
+            app = Run("Rails + React App")
+            cicd >> app >> SQL("database")
+        
+    User("developer") >> Github("code push") >> dev
+    Client("client") >> app
